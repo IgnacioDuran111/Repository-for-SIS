8d0abdf11875cdc23d96a2f02c23e60ca9e7dbca
nAachO7 <130947156+nAachO7@users.noreply.github.com>
2024-08-18 18:58:17 -0400

    13vo ejercicio terminado

--- Ejercicios_01_13.cpp
+++ Ejercicios_01_13.cpp
@@ -3,11 +3,20 @@ Materia: Programación I, Paralelo 1
 Autor: Ignacio Joel Durán Choque
 Fecha creación: 18/08/20
 Número de ejercicio: 13
-Problema planteado: Crea un programa para leer dos números y verificar si uno es múltiplo del otro o no lo es.
+Problema planteado: Crea un programa para leer un número entero y mostrarlo con las cifras al revés.
 */
 #include<iostream>
 using namespace std;
 int main(){
     system("cls");
+    int number, rest, invert=0;
+    cout<<"THIS PROGRAM INVERT A NUMBER"<<endl;
+    cin>>number;
+    while(number>0){
+        rest=number%10;
+        invert=invert*10+rest;
+        number/=10;
+    }
+    cout<<"THE INVERT NUMBER IS "<<invert<<endl;
     return 0;
 }
