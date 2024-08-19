df14a565d67c8dad8542fc21ed2b437e5b80c648
nAachO7 <130947156+nAachO7@users.noreply.github.com>
2024-08-18 23:25:42 -0400

    17vo ejercicio corregido

--- Ejercicio_01_17.cpp
+++ Ejercicio_01_17.cpp
@@ -3,43 +3,24 @@ Materia: Programación I, Paralelo 1
 Autor: Ignacio Joel Durán Choque
 Fecha creación: 18/08/20
 Número de ejercicio: 15
-Problema planteado: Escribe una función que calcule si un número dado es un número de Armstrong
-                    (o también llamado narcisista). 
-                    Si no conoces qué es un número de Armstrong, debes buscar información al respecto.
+Problema planteado: Crea un programa se encargue de transformar un número decimal a binario
+                    sin utilizar funciones propias del lenguaje que lo hagan directamente.
 */
 #include<iostream>
 using namespace std;
 int main(){
     system("cls");
-    int number, digits, aux, keep_number, result,power=1;
-    cout<<"THIS PROGRAM CHECKS IF A NUMBER IS NARCISSISTIC"<<endl;
-    cout<<"PLEASE ENTER A NUMBER"<<endl;
+    int number,aux, unit, binary=0, cont=1;
+    cout<<"THIS PROGRAM TRANSFORM A NUMBER FROM DECIMAL TO BINARY"<<endl;
+    cout<<"PLEASE, ENTER A NUMBER"<<endl;
     cin>>number;
-    aux=number;//variable auxiliar 
-    keep_number=number; //guarda el número para comparar al final
-    //Utilizar la variable auxiliar para saber los n digitos del número
-    while(aux>0){
-        aux/=10;
-        digits++;
+    aux=number;
+    while (number>0){
+        unit=number%2;
+        binary+=unit*cont;
+        cont*=10;
+        number/=2;
     }
-
-    while(number>0){
-        //Recorrer los dígitos del número y elevarlos a la potencia del mismo
-        for(int i=1;i<=digits;i++){
-            power*=number%10;
-        }
-        //sumar las potencias
-        result+=power;
-        power=1;
-        //recorrer el siguiente número
-        number/=10;
-    }
-
-    if (result==keep_number)
-        cout<<keep_number<<" IS A NARCISSISTIC NUMBER";
-    
-    else
-        cout<<keep_number<<" IS'NT A NARCISSISTIC NUMBER "<<result;
-
+    cout<<"THE NUMBER "<<aux<<" IS IN BINARY: "<<binary<<endl;
     return 0;
 }
